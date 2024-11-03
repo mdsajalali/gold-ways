5aa4979179f6fc89a0e9a68b6be157c078b5a1da
mdsajalali <mdsajalali2020@gmail.com>
2024-11-03 19:54:06 +0600

    add: bottom navigation

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div className="flex items-center justify-center py-10 text-2xl uppercase">Dashboard is coming soon!</div>;
+};
+
+export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navigation from "@/components/core/Navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Navigation />
       </body>
     </html>
   );
