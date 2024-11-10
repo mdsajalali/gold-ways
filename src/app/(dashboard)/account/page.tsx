9012da4d1eb48d547d141768e7c7c1ab06bb15e3
mdsajalali <mdsajalali2020@gmail.com>
2024-11-10 22:14:44 +0600

    update: sidebar

--- src/app/(dashboard)/account/page.tsx
+++ src/app/(dashboard)/account/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CalendarDaysIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   CubeIcon,
   UserGroupIcon,
@@ -60,13 +60,62 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function page() {
+export default function Page() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-  // Function to toggle sidebar visibility
-  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+  // Handle opening/closing the sidebar
+  const toggleSidebar = () => {
+    setSidebarOpen((prevState) => !prevState);
+  };
+
+  // Close sidebar when Escape key is pressed
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setSidebarOpen(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
+  // Handle overlay click to close the sidebar
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.currentTarget === event.target) {
+      setSidebarOpen(false);
+    }
+  };
+
   return (
     <>
+      {/* Button to toggle the sidebar */}
+      <div className="flex items-center justify-end">
+        <button
+          onClick={toggleSidebar}
+          className="p-4 text-gray-600 hover:text-amber-600 md:hidden"
+        >
+          <span className="sr-only">Open Sidebar</span>
+          {/* Hamburger menu icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+
       <dl className="container mx-auto flex flex-wrap">
         {/* Name and Phone */}
         <div className="flex-auto pl-6 pt-6">
@@ -120,75 +169,17 @@ export default function page() {
       {/* Transactions History */}
       <TransactionsHistory />
 
-      {/* TODO: The previous code */}
-      {/* <aside className="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-20">
-        <nav className="flex-none px-4 sm:px-6 lg:px-0">
-          <ul
-            role="list"
-            className="flex flex-col gap-x-3 gap-y-1 whitespace-nowrap"
-          >
-            {secondaryNavigation.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-50 text-amber-600"
-                      : "text-gray-700 hover:bg-gray-50 hover:text-amber-600",
-                    "group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm font-semibold leading-6",
-                  )}
-                >
-                  <item.icon
-                    aria-hidden="true"
-                    className={classNames(
-                      item.current
-                        ? "text-amber-600"
-                        : "text-gray-400 group-hover:text-amber-600",
-                      "h-6 w-6 shrink-0",
-                    )}
-                  />
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </aside> */}
-
+      {/* Sidebar */}
       <div className="relative py-20">
-        {/* Mobile Button to toggle sidebar */}
-        <button
-          onClick={toggleSidebar}
-          className="p-4 text-gray-600 hover:text-amber-600 lg:hidden"
-        >
-          <span className="sr-only">Open Sidebar</span>
-          {/* Button Icon (hamburger menu) */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-
-        {/* Sidebar */}
         <aside
-          className={`fixed inset-0 z-50 transform bg-white   transition-all duration-300 ease-in-out ${
+          className={`fixed inset-0 z-50 transform bg-white transition-all duration-300 ease-in-out ${
             isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-          } w-[75%] lg:relative lg:block lg:w-64 lg:translate-x-0 lg:border-0 lg:py-20`}
+          } w-[75%] md:relative md:block md:w-64 md:translate-x-0 md:border-0 md:py-20`}
         >
           {/* Close Button on Mobile */}
           <button
             onClick={toggleSidebar}
-            className="absolute right-4 top-4 p-2 text-gray-600 hover:text-amber-600 lg:hidden"
+            className="absolute right-4 top-4 p-2 text-gray-600 hover:text-amber-600 md:hidden"
           >
             <span className="sr-only">Close Sidebar</span>
             <svg
@@ -239,6 +230,14 @@ export default function page() {
             </ul>
           </nav>
         </aside>
+
+        {/* Overlay to close sidebar */}
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 lg:hidden"
+            onClick={handleOverlayClick}
+          ></div>
+        )}
       </div>
     </>
   );
