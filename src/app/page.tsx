a7cd41f2bd5b1c060785c1043e2110a3fef3be02
rahathosen <rahathosen.me@gmail.com>
2024-11-26 21:20:53 +0600

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,14 @@ import Link from "next/link";
 const page = () => {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center">
-      {/* Background Image */}
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('/gold_bg.jpg')" }}
-      />
-      {/* Overlay with background color and opacity */}
-      <div className="absolute inset-0 bg-black opacity-85" />
-
       {/* Content */}
       <div className="relative z-10 text-center">
-        <h1 className="mb-2 text-5xl font-bold text-yellow-500">
-          GOLD WAYS<sup>®</sup>
+        <h1 className="mb-2 text-5xl font-[var(--font-unna)] text-stone-900">
+          WAYS GOLD<sup>®</sup>
         </h1>
-        <p className="text-sm text-gray-400">
-          Powered by &apos;AL-HASQAN GOLD BANK&apos;
+        <p className="text-xs text-zinc-600">
+          Powered by{" "}
+          <span className="font-bold">&apos;AL-HASQAN GOLD BANK&apos;</span>
         </p>
       </div>
       <br />
@@ -27,18 +20,17 @@ const page = () => {
       <Link href={"/login"}>
         <Button
           variant="default"
-          className="relative z-10 w-full duration-300 hover:bg-yellow-500 hover:text-black sm:w-auto"
+          className="relative z-10 w-full bg-sand-lite font-medium text-black duration-300 hover:bg-sand-deep hover:text-black sm:w-auto"
         >
           Get started
           <ArrowRight className="ml-2 size-4" />
         </Button>
       </Link>
 
-      {/* Gold sparkles (Optional: if you want small decorations) */}
       <div className="absolute bottom-0 left-0 flex space-x-2 p-4">
-        <div className="h-2 w-2 animate-pulse rounded-full bg-yellow-500"></div>
-        <div className="h-3 w-3 animate-pulse rounded-full bg-yellow-500 delay-200"></div>
-        <div className="delay-400 h-1 w-1 animate-pulse rounded-full bg-yellow-500"></div>
+        <div className="h-2 w-2 animate-pulse rounded-full bg-sand-deep"></div>
+        <div className="h-3 w-3 animate-pulse rounded-full bg-sand-deep delay-200"></div>
+        <div className="delay-400 h-1 w-1 animate-pulse rounded-full bg-sand-lite"></div>
       </div>
     </div>
   );
